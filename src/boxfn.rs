8c9341938ad4a17758cbccff146e88f19140581d
QuietMisdreavus <grey@quietmisdreavus.net>
2018-09-19 11:41:40 -0500

    initial HandlerMap impl, with BoxFn

---
+++ src/boxfn.rs
@@ -0,0 +1,91 @@
+use std;
+use std::marker::PhantomData;
+
+// Kinda important that you can't make values of these.
+// But unstable, so we shim it with something else.
+// extern { pub type Opaque; }
+pub struct Opaque(());
+
+struct BoxFnVtable<A: ?Sized, F: ?Sized = Opaque> {
+	call: fn(&F, A),
+	drop_box: unsafe fn(*mut F),
+}
+
+pub struct BoxFn<'a, A: 'a + ?Sized, F: 'a + ?Sized = Opaque> {
+	data: &'a mut F,
+	vtable: &'a BoxFnVtable<A, F>,
+	_invariant: PhantomData<&'a mut &'a ()>,
+}
+
+impl<'a, A: ?Sized, F: ?Sized> Drop for BoxFn<'a, A, F> {
+	fn drop(&mut self) {
+		unsafe {
+			(self.vtable.drop_box)(self.data);
+		}
+	}
+}
+
+impl<'a, A, F: Fn(A)> From<Box<F>> for BoxFn<'a, A, F> {
+	fn from(f: Box<F>) -> Self {
+		unsafe fn drop_box<F>(f: *mut F) {
+			drop(Box::from_raw(f));
+		}
+		fn call<F: Fn(A), A>(f: &F, arg: A) {
+			f(arg)
+		}
+		BoxFn {
+			data: unsafe { &mut *Box::into_raw(f) },
+			vtable: &BoxFnVtable {
+				call,
+				drop_box,
+			},
+			_invariant: PhantomData,
+		}
+	}
+}
+
+impl<'a, A, F> BoxFn<'a, A, F> {
+	pub fn erase(self) -> BoxFn<'a, A> {
+		unsafe {
+			let data = &mut *(self.data as *mut _ as *mut Opaque);
+			let vtable = &*(self.vtable as *const _ as *const BoxFnVtable<A>);
+			std::mem::forget(self);
+			BoxFn {
+				data,
+				vtable,
+				_invariant: PhantomData,
+			}
+		}
+	}
+}
+
+impl<'a, A> BoxFn<'a, A> {
+	pub fn erase_arg(self) -> BoxFn<'a, Opaque> {
+		unsafe {
+			let data = &mut *(self.data as *mut _);
+			let vtable = &*(self.vtable as *const _ as *const BoxFnVtable<Opaque>);
+			std::mem::forget(self);
+			BoxFn {
+				data,
+				vtable,
+				_invariant: PhantomData,
+			}
+		}
+	}
+}
+
+impl<'a, A, F: ?Sized> BoxFn<'a, A, F> {
+	#[allow(dead_code)]
+	pub fn call(&self, arg: A) {
+		(self.vtable.call)(self.data, arg);
+	}
+}
+
+impl<'a> BoxFn<'a, Opaque> {
+	pub unsafe fn call_erased<A: 'a>(&self, arg: A) {
+		std::mem::transmute::<
+			fn(&Opaque, Opaque),
+			fn(&Opaque, A),
+		>(self.vtable.call)(self.data, arg);
+	}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,86 @@
+use std::any::{Any, TypeId};
+use std::collections::HashMap;
+
+mod boxfn;
+
+use boxfn::{BoxFn, Opaque};
+
+/// Struct that maps types with functions or closures that can receive them.
+#[derive(Default)]
+pub struct HandlerMap(HashMap<TypeId, BoxFn<'static, Opaque>>);
+
+impl HandlerMap {
+    /// Creates a new map with no handlers.
+    pub fn new() -> HandlerMap {
+        Self::default()
+    }
+
+    /// Insert a new callable into the map.
+    pub fn insert<T: Any, F: Fn(T) + 'static>(&mut self, callable: F) {
+        let ptr: BoxFn<'static, T, F> = Box::new(callable).into();
+        let ptr: BoxFn<'static, Opaque> = ptr.erase().erase_arg();
+        let id = TypeId::of::<T>();
+
+        self.0.insert(id, ptr);
+    }
+
+    /// Calls the callable with the given message, returning whether the callable was registered.
+    pub fn call<T: Any>(&self, msg: T) -> bool {
+        let id = TypeId::of::<T>();
+        if let Some(act) = self.0.get(&id) {
+            unsafe { act.call_erased(msg); }
+            true
+        } else {
+            false
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::HandlerMap;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        struct MyMessage;
+        fn respond(_: MyMessage) {}
+
+        let mut map = HandlerMap::new();
+        map.insert(respond);
+
+        assert!(map.call(MyMessage));
+    }
+
+    #[test]
+    fn no_handler() {
+        struct MyMessage;
+
+        let map = HandlerMap::new();
+
+        assert!(!map.call(MyMessage));
+    }
+
+    #[test]
+    fn handler_is_called() {
+        use std::sync::Arc;
+        use std::sync::atomic::AtomicUsize;
+        use std::sync::atomic::Ordering::SeqCst;
+
+        let mut map = HandlerMap::new();
+
+        struct FancyCaller;
+        let acc = Arc::new(AtomicUsize::new(0));
+        {
+            let acc = acc.clone();
+            map.insert(move |_: FancyCaller| {
+                acc.fetch_add(1, SeqCst);
+            });
+        }
+
+        map.call(FancyCaller);
+        map.call(FancyCaller);
+        map.call(FancyCaller);
+
+        assert_eq!(acc.load(SeqCst), 3);
     }
 }
