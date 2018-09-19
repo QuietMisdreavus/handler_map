81086c357b04d74d3ecb9ff10acbb91355c76ea4
QuietMisdreavus <grey@quietmisdreavus.net>
2018-09-19 14:05:15 -0500

    add license headers

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL was
+// not distributed with this file, You can obtain one at
+// http://mozilla.org/MPL/2.0/.
+
 //! Crate which contains a "handler map", a structure that maps message types with "handlers" that
 //! can be called with them.
 //!
